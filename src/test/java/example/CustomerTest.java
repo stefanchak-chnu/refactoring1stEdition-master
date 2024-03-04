c0b764ded794a95fc5ea7baf13111886aa854898
gprohorov <g.prokhorov@indevlab.com>
2024-03-04 08:36:24 +0200

    initial commit

---
+++ src/test/java/example/CustomerTest.java
@@ -0,0 +1,12 @@
+package example;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static example.Movie.MovieType.*;
+import static org.junit.Assert.assertEquals;
+
+public class CustomerTest {
+
+}
